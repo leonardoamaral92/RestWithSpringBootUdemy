6252deda49f2306d78fb2bd03275dac9b6f6a27d
Leonardo <leonardo.amaral92@gmail.com>
2021-05-24 12:02:32 -0300

    Trabalhando com Migrations usando o Flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` VALUES
+(2,'Uberlândia - Minas Gerais - Brasil','Gabriela','Female','Costa'),
+(3,'Niterói - RJ - Brasil','Rafael','Male','Silva'),
+(6,'Búzios - RJ - Brasil','Rodrigo','Male','Maia'),
+(7,'Uberlândia - Minas Gerais - Brasil','Geléia','Male','Costa'),
+(8,'Uberlândia - Minas Gerais - Brasil','Geléia','Male','Costa'),
+(9,'Uberlândia - Minas Gerais - Brasil','Geléia','Male','Costa'),
+(10,'Uberlândia - Minas Gerais - Brasil','Geléia','Male','Costa');
